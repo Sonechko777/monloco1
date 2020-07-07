90f12517caec0926542de73eaf94f38d6c37c333
Sonechko777 <64007767+Sonechko777@users.noreply.github.com>
2020-07-07 04:50:47 +0300

    Mocloco
    Monloco

---
+++ index.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style/style.css"
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="fonts/sunset-serial-bold.ttf"
+
+    <title></title>
+</head>
+<body>
+<div class="nav">
+        <div class="logo">
+            <div class="logo_up">
+                monloco
+            </div>
+            <div class="logo_down">
+                BAEAUTY COWORKING
+            </div>
+        </div>
+
+        <div class="nav_right">
+            <div class="drop_ct">
+                <div class="dropdown">
+                    <button class="btn_btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Москва
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <button class="dropdown-item" type="button">CT</button>
+                        <button class="dropdown-item" type="button">Another CT</button>
+                        <button class="dropdown-item" type="button">MORE CT</button>
+                    </div>
+                </div>
+            </div>
+            <div class="login" style="margin-left: 30px">
+                <div class="avatar" ><img src="img/avatar.png"> </div>
+                <div class="nick_name"></div>
+            </div>
+        </div>
+</div>
+<div class="main" style="background:#fdf8f4;">
+  
+    <div class="all_cards">
+        
+
+        <div class="main_up">
+            <form>
+                <div class="search">
+                    <input type="search" class="form_input_search" placeholder="ФИО мастера" name="q">
+                    <input type="submit" class="form_submit_search" value="">
+                </div>
+            </form>
+        <div class="right_dr_1">
+            <div class="dropdown">
+                <button style="font-style: normal;font-weight: 600;font-size: 16px;line-height: 20px; color: #383838; " class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Фильтры
+                </button>
+
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">Фильтры</a>
+                    <a class="dropdown-item" href="#">Еще фильтры</a>
+                    <a class="dropdown-item" href="#">Еще немного фильтров</a>
+                </div>
+            </div>
+            </div>
+            <div class="right_dr_2">
+                <div class="dropdown" style="margin-left: 30px;">
+                    <button class="btn_btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Расстояние
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <button class="dropdown-item" type="button">CT</button>
+                        <button class="dropdown-item" type="button">Another CT</button>
+                        <button class="dropdown-item" type="button">MORE CT</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right_dr_3">
+                <div class="btn-group dropup" style="background: #FFFFFF;
+/* #DBBFA7 */
+
+border: 2px solid #DBBFA7;
+box-sizing: border-box;
+border-radius: 5px;">
+                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 160px; height: 50px; background: #FFFFFF; font-weight: 600;
+font-size: 16px;
+line-height: 20px;
+/* identical to box height */
+
+
+/* Style */
+
+color: #383838;">
+                        Цена
+                    </button>
+                    <div class="dropdown-menu">
+                        <button class="dropdown-item" type="button">CT</button>
+                        <button class="dropdown-item" type="button">Another CT</button>
+                        <button class="dropdown-item" type="button">MORE CT</button>
+                        <!-- Dropdown menu links -->
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+        
+        <div class="card" style="flex-direction: row">
+            <div class="card_left">
+
+            </div>
+            <div class="card_right">
+                <div class="card_title">
+                    Название коворкинга
+                </div>
+                <div class="card_cab">
+                    Кабинеты:
+                </div>
+                <div class="card_time">
+                    Время работы:
+                </div>
+                <button class="card_button">
+                    Подробнее
+                </button>
+
+
+            </div>
+        </div>
+        <div class="card" style="flex-direction: row">
+            <div class="card_left">
+
+            </div>
+            <div class="card_right">
+                <div class="card_title">
+                    Название коворкинга
+                </div>
+                <div class="card_cab">
+                    Кабинеты:
+                </div>
+                <div class="card_time">
+                    Время работы:
+                </div>
+                <button class="card_button">
+                    Подробнее
+                </button>
+
+
+            </div>
+        </div>
+        <div class="card" style="flex-direction: row">
+            <div class="card_left">
+
+            </div>
+            <div class="card_right">
+                <div class="card_title">
+                    Название коворкинга
+                </div>
+                <div class="card_cab">
+                    Кабинеты:
+                </div>
+                <div class="card_time">
+                    Время работы:
+                </div>
+                <button class="card_button">
+                    Подробнее
+                </button>
+
+
+            </div>
+        </div>
+        <div class="card" style="flex-direction: row">
+            <div class="card_left">
+
+            </div>
+            <div class="card_right">
+                <div class="card_title">
+                    Название коворкинга
+                </div>
+                <div class="card_cab">
+                    Кабинеты:
+                </div>
+                <div class="card_time">
+                    Время работы:
+                </div>
+                <button class="card_button">
+                    Подробнее
+                </button>
+
+
+            </div>
+        </div>
+        <div class="card" style="flex-direction: row">
+            <div class="card_left">
+
+            </div>
+            <div class="card_right">
+                <div class="card_title">
+                    Название коворкинга
+                </div>
+                <div class="card_cab">
+                    Кабинеты:
+                </div>
+                <div class="card_time">
+                    Время работы:
+                </div>
+                <button class="card_button">
+                    Подробнее
+                </button>
+
+
+            </div>
+        </div>
+        <div class="card" style="flex-direction: row">
+            <div class="card_left">
+
+            </div>
+            <div class="card_right">
+                <div class="card_title">
+                    Название коворкинга
+                </div>
+                <div class="card_cab">
+                    Кабинеты:
+                </div>
+                <div class="card_time">
+                    Время работы:
+                </div>
+                <button class="card_button">
+                    Подробнее
+                </button>
+
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<div class="footer">
+    <div class="footer_up">
+        <div class="logo_footer">
+            <div class="footer_logo_up">
+             monloco
+             </div>
+            <div class="footer_logo_down">
+            BAEAUTY COWORKING
+            </div>
+        </div>
+    </div>
+    <hr style="border: 1px solid rgba(255, 255, 255, 0.5);">
+    <div class="footer_down">
+        <div class="footer_down_l">
+                <div class="menu_1" style="font-weight: 500;font-size: 16px;line-height: 20px;color: #FFFFFF; ">
+                    <ul style="list-style-type: none; margin-left: 90px; text-decoration:none;">
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Кворкинги </a></li>
+                    </ul>
+                </div>
+                <div class="menu_2" style="margin-left: 164px; font-weight: 500;font-size: 16px;line-height: 20px;color: #FFFFFF;">
+                    <ul style="list-style-type: none; text-decoration:none;">
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Мастера </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Мои мастера </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Мои записи </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                    </ul>
+                </div>
+                <div class="menu_3" style="margin-left: 159px; font-weight: 500;font-size: 16px;line-height: 20px;color: #FFFFFF;">
+                    <ul style="list-style-type: none; text-decoration:none;">
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Меню </a></li>
+                        <li style="margin-bottom: 15px;" ><a href="#" style="text-decoration:none; color: #FFFFFF !important;">Кворкинги </a></li>
+                    </ul>
+                </div>
+        </div>
+
+
+        <div class="footer_down_r" >
+            <div class="menu_4">
+                <div class="menu_4_up">
+                    <span class="menu_4_up_span">Контактная информация</span>
+                </div>
+                <div class="menu_4_down">
+                    <div class="footer_num">
+                        +7 (495) 123-45-67
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
